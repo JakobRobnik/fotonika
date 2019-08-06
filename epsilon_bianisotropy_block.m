a66f0664bf0820903ec1886f633a18c80dd3170f
JakobRobnik <43053552+JakobRobnik@users.noreply.github.com>
2019-08-06 12:51:11 +0200

    Delete epsilon_bianisotropy_block.m

--- epsilon_bianisotropy_block.m
+++
@@ -1,9 +0,0 @@
-function eps_chi_chi_mu = epsilon_bianisotropy_block(pdir,lambda2)
-%THIS FUNCTION CALLS A FUNCTION TO BUILD EPSILON MATRIX AND BIANISOTTROPY MATRIX AND JOINS THEM IN
-%A MATRIX i*[-EPSILON, BIANISOTROPY; BIANISOTROPY, MU]- 
-global nx ny nz NO DN NOUT dirfield file R c DPMLs DPMLe gain;
-[epsilon, mu] = epsilon_S_block(pdir,lambda2);
-bianisotropy = sparse(nx*ny*nz*3, nx*ny*nz*3);
-eps_chi_chi_mu = i* [-epsilon, bianisotropy; bianisotropy, mu];
-
-end
